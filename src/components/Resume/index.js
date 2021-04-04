c8f7abf717daf7f9fcd8984d6d7b14a4b850043f
Matthew Damron <mattdamron@msn.com>
2021-04-04 13:38:10 -0600

    added more skills

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,19 +8,28 @@ function Resume() {
     const backendSkills = [
         `API's`, 'Node', 'Express', 'MySql, Sequelize', 'MongoDB, Mongoose'
     ]
+    const otherPrograming = [
+        'C#', 'VB.net', 'iLogic(VB)', 'AutoLisp(LISP)', 'Git'
+    ]
     
     return (
         <section>
             <h3>Resume</h3>
                 Download My <a href={resumePDF} download>Resume</a>
             <div>
-                <h4 className='mt-4'>Front-end Skills</h4>
+                <h4 className='mt-4'>Programing Skills</h4>
+                {otherPrograming.map((skills) => (
+                    <ul>
+                        <li>{skills}</li>
+                    </ul>
+                ))}
+                <h4 className='mt-4'>Web Front-end Skills</h4>
                 {frontendSkills.map((skills) => (
                     <ul>
                         <li>{skills}</li>
                     </ul>
                 ))}
-                <h4 className='mt-4'>Back-end Skills</h4>
+                <h4 className='mt-4'>Web Back-end Skills</h4>
                 {backendSkills.map((skills) => (
                     <ul>
                         <li>{skills}</li>
